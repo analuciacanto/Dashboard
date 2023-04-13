113a7675d4eb490c3a6cf54b93af252a3f35d0f4
Ana Lúcia Canto <31079626+analuciacanto@users.noreply.github.com>
2023-04-13 12:35:49 -0300

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,14 @@
-
+const settings = {
+    DASHBOARD_NAME: 'Dashboard  UFRJ',
+    BROKER_PROTOCOL: 'ws',
+    BROKER_IP: 'k3s.cos.ufrj.br',
+    BROKER_PORT: 30364,
+    BROKER_URL_PATH: '/ws',
+    BROKER_LOGIN: 'admin',
+    BROKER_PASSWORD: '.123qwe',
+    ALERTS_TOPIC: 'alerts',
+    RECORDS_TO_SAVE: 500,
+    MINUTES_TO_EXPIRE: 1,   
+    REPORT_INTERVAL_MINUTES: 60,
+  };
+  
